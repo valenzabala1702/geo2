668ab50db0915c03fc2c513b0a73227f2bdd1e5a
Usuario <usuario@MacBook-Pro-de-administrador.local>
2026-02-02 14:11:09 -0500

    third commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,17 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    
     return {
       server: {
         port: 3000,
         host: '0.0.0.0',
+        // Se añade esta sección para permitir el acceso desde Render y Local
+        allowedHosts: [
+          'geo2-huqr.onrender.com', // Tu dominio específico
+          'localhost',              // Desarrollo local
+          '.onrender.com'           // Cualquier variante de subdominio de Render
+        ]
       },
       plugins: [react()],
       define: {
